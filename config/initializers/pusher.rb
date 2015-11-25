41d85f338e915d55b521613db327f43e7178893a
MARISA <mnabril3@gmail.com>
2015-11-24 20:35:49 -0800

    updated pusher.rb

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,18 +1,18 @@
 require 'pusher'
-# HEROKU PUSHERS KEYS #
+# HEROKU PUSHER KEYS #
 # Pusher.app_id = '153880'
 # Pusher.key = '0b9f3939aed04fe7f958'
 # Pusher.secret = '80e890872005954d306e'
 
 # Development
-Pusher.app_id = '153881'
-Pusher.key = 'f718bef729fd9d06149e'
-Pusher.secret = '55d1eb74e4870e0f75f4'
+# Pusher.app_id = '153881'
+# Pusher.key = 'f718bef729fd9d06149e'
+# Pusher.secret = '55d1eb74e4870e0f75f4'
 
 # MARISA'S LOCAL PUSHER KEYS #
- # Pusher.app_id = '151986'
- # Pusher.key    = 'ff9aac51168e1f42f5ec'
- # Pusher.secret = '4edf8edf885578f7de19'
+ Pusher.app_id = '151986'
+ Pusher.key    = 'ff9aac51168e1f42f5ec'
+ Pusher.secret = '4edf8edf885578f7de19'
 
 
 Pusher.logger = Rails.logger
