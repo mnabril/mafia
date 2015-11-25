8d7b1330a21c01ad0d197e30cee895177ecf3b33
MARISA <mnabril3@gmail.com>
2015-11-24 16:20:31 -0800

    slowly fixing things

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,3 @@
 class Game < ActiveRecord::Base
+	attr_accessor :gameID, :num_of_players, :max_players
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'games#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  #root 'chat_messages#index'
+  resources :chat_messages, only: [:create]
+  get '/chat_messages/' => 'chat_messages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
